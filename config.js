805ae01414324df94016763b9093f4c819237efa
Andywang2007 <wangshumingnb@163.com>
2026-06-02 13:26:30 +0800

    Configure Baidu map API key

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.LOVE_MAP_SUPABASE = {
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmZ2hxc2RwZmlkdmt2c25kdGN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzNTM5OTYsImV4cCI6MjA5NTkyOTk5Nn0.9rzqnvr0c2GayPViav03fs0xoxhF2uzlVBT5FOcZAeU",
   tableName: "meeting_records",
   photoBucket: "meeting-photos",
-  baiduAk: ""
+  baiduAk: "FGXpNRuugg2t2YiMrCLDMl1VY530MVVK"
 };
